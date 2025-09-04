92c1e5dbfef1a76d6d97fe43be4d11ec079921d6
Arruda98 <jeffersonarruda98@gmail.com>
2025-09-03 23:16:09 -0300

    Sorteio Hot Wheels

---
+++ evento.sorteio.minis/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Evento
+{
+    class Sorteio()
+    {
+        public static List<int> Number = new List<int>();
+        public static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("O sorteio vai começar");
+
+            Console.WriteLine("Quantos números foram sorteados");
+            int quantidadeSorteada = int.Parse(Console.ReadLine());
+
+            double quantidade = 0;
+
+            for (int i = 0; i < quantidadeSorteada; i++)
+            {
+                Random random = new Random();
+
+                int Number = random.Next(1, 100);
+
+            }
+
+            Console.WriteLine(Number.Count());
+            
+        }
+    }
+}
